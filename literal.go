b79998ad1bb5d1cf0943088626066f65160624b1
mthlpbs <100818591+mthlpbs@users.noreply.github.com>
2025-12-09 23:14:28 +0530

    fix: update import path to correct repository location

--- literal.go
+++ literal.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/taciturnaxolotl/markscribe/literal"
+import "github.com/mthlpbs/markscribe/literal"
 
 func literalClubCurrentlyReading(count int) []literal.Book {
 	books, err := literal.CurrentlyReading()
